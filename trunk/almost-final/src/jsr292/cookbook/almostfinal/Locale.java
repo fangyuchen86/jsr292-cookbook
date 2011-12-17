841632061c1985d141cff6ee531ac9f0e45a8cfd
remi.forax <remi.forax@ec5aeeb3-d8e4-83a8-37b4-e3db1dd76c75>
2011-12-17 17:39:55 +0000

    use getDefault/setDefault instead of getDefaultResource/setDefaultResource

--- trunk/almost-final/src/jsr292/cookbook/almostfinal/Locale.java
+++ trunk/almost-final/src/jsr292/cookbook/almostfinal/Locale.java
@@ -22,7 +22,7 @@ public class Locale {
       };
   private static final MethodHandle DEFAULT_LOCALE_GETTER = DEFAULT_LOCALE.createGetter();
   
-  public static Locale getDefaultResource() {
+  public static Locale getDefault() {
     try {
       return (Locale)(Object)DEFAULT_LOCALE_GETTER.invokeExact();
     } catch (Throwable e) {
@@ -30,21 +30,21 @@ public class Locale {
     }
   }
   
-  public static void setDefaultResource(Locale resource) {
-    DEFAULT_LOCALE.setValue(resource);
+  public static void setDefault(Locale locale) {
+    DEFAULT_LOCALE.setValue(locale);
   }
   
   public static void main(String[] args) throws InterruptedException {
     new Thread(new Runnable() {
       @Override
       public void run() {
-        while(getDefaultResource().getName() == "default") {
+        while(getDefault().getName() == "default") {
           // do nothing
         }
-        System.out.println(getDefaultResource().getName());
+        System.out.println(getDefault().getName());
       }
     }).start();
     Thread.sleep(2000);
-    setDefaultResource(new Locale("foo"));
+    setDefault(new Locale("foo"));
   }
 }
